db05bd5d022e1440141d8eac5d080b08061a2b36
Kayla C <itskaychay@gmail.com>
2023-06-30 00:39:32 -0400

    Updated login, signup, and main view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState(""); 
+    const [password, setPassword] = useState(""); 
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            access: username, 
+            secret: password
+        };
+
+        // fetch api
+        fetch("https://myflix-kc.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.jason())
+        .then((data) => {
+            console.log("Login response: ", data);
+            if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user)); // stores user data
+                localStorage.setItem("token", data.token); // stores token
+                onLoggedIn(data.user, data.token);
+            } else {
+                alert("No such user.")
+            }
+        })
+        .catch((e) => {
+            alert("Something went wrong.")
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
 
-const [selectedMovie, setSelectedMovie] = useState(null);
 
-// fetch movie api
 useEffect(() => {
-    fetch("https://myflix-kc.herokuapp.com/movies")
+    if (!token) {
+        return;
+    }
+    // fetch movie api
+    fetch("https://myflix-kc.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` }
+    })
         .then((response) => response.json())
         .then((data) => {
+            setMovies(movies);
+            console.log(data);
+
             // process the api response and update the movies state
             const moviesFromApi = data.map((movie) => {
                 return {
@@ -27,20 +41,34 @@ useEffect(() => {
 
             setMovies(moviesFromApi);
         });
-}, []);
+}, [token]);
 
+// user is not signed in loads login or signup view
+if (!user) {
+    return (
+        <>
+        <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+        }}/>
+        or
+        <SignupView />
+        </>
+    );
+}
+
+
+// brings user back to the movie selection
 if (selectedMovie) {
     return (
         <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
 }
 
+// no movie will show that the list is empty
 if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-}
-
-return (
-    <div>
+    return (
+        <>
         {movies.map((movie) => (
             <MovieCard
                 key={movie.id}
@@ -50,6 +78,31 @@ return (
                 }}
             />
         ))}
+            <button
+                onClick={() => {
+                    setUser(null);
+                }}
+                >
+                    Logout
+            </button>
+            <div>The list is empty!</div>;
+        </>
+    );
+}
+
+// nullifies token when user logs out
+return (
+    <div>
+        <button
+            onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear();
+            }}
+            >
+                Logout
+        </button>
+        
     </div>
     );
 };
